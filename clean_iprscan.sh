0a2808f1a9a0aa26747d217342921bc661a2d0ee
Victor Flores <vflorelo@www.atglabs.org>
2025-02-24 22:39:34 +0000

    removed compress option, it was dumb anyway

--- clean_iprscan.sh
+++ clean_iprscan.sh
@@ -4,11 +4,11 @@ function usage(){
     echo
     echo "Options:"
     echo "  --tsv_file -> TSV file with InterProScan results"
+    echo "  --e_value  -> E-value threshold for keeping significant domains"
     echo "  --out_file -> TSV file stripped down to be analysed with HyPE"
-    echo "  --compress -> Compresses the output file [TRUE]/FALSE"
     echo
     echo "Example:"
-    echo "  clean_iprscan.sh --tsv_file /path/to/my/interproscan/results.tsv --out_file /path/to/my/interproscan/clean_results.tsv --compress TRUE"
+    echo "  clean_iprscan.sh --tsv_file /path/to/my/interproscan/results.tsv --out_file /path/to/my/interproscan/clean_results.tsv --e_value 1e-6"
     echo
 	}
 export -f usage
@@ -19,13 +19,13 @@ do
             shift
             tsv_file=$(realpath $1)
             ;;
+        --e_value  )
+            shift
+            e_value=$1
+            ;;
         --out_file )
             shift
             out_file=$1
-            ;;
-        --compress )
-            shift
-            compress=$1
             ;;
 		--help     )
             usage
@@ -34,35 +34,20 @@ do
 	esac
 	shift
 done
-
 if [ ! -f "${tsv_file}" ] || [ -z "${tsv_file}" ]
 then
     echo "Missing input file name. Exiting"
     exit 1
 fi
-
+if [ -z "${e_value}" ]
+then
+    echo "Missing e-value cutoff. Exiting"
+    exit 1
+fi
 if [ -z "${out_file}" ]
 then
     echo "Missing output file name. Exiting"
     exit 1
 fi
-
-if [ "${compress}" != "TRUE" ] && [ "${compress}" != "true" ] && [ "${compress}" != "FALSE" ] && [ "${compress}" != "false" ]
-then
-    echo "Invalid --compress value. Exiting"
-    exit 1
-fi
-
-datablock=$(awk 'BEGIN{FS="\t";OFS="\t";dot="."}{print $1,dot,dot,$4,$5,$6,dot,dot,dot,dot,dot,$12,$13}' ${tsv_file} | sort -V | uniq)
-
-if [ "${compress}" == "TRUE" ] || [ "${compress}" == "TRUE" ]
-then
-    gz_test=$(echo "${out_file}" | grep -c gz$)
-    if [ "${gz_test}" -eq 0 ]
-    then
-        out_file=$(echo "${out_file}.gz")
-    fi
-    echo "${datablock}" | gzip --best --stdout > ${out_file}
-else
-    echo "${datablock}" > ${out_file}
-fi
+datablock=$(awk -v e_value="${e_value}" 'BEGIN{FS="\t";OFS="\t";dot="."}{if($9<=e_value){print $1,dot,dot,$4,$5,$6,dot,dot,dot,dot,dot,$12,$13}}' ${tsv_file} | sort -V | uniq)
+echo "${datablock}" > ${out_file}
